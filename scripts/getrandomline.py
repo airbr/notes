2cc0120ccff42f6aaf01a188eae4de468b1bb955
Morgan Murrah <morgan@easyrxcloud.com>
2020-01-06 16:56:40 -0800

    Move scripts

--- scripts/getrandomline.py
+++ scripts/getrandomline.py
@@ -1,5 +1,6 @@
 import random
+
 def random_line(fname):
     lines = open(fname).read().splitlines()
     return random.choice(lines)
-print(random_line('edited/Law notes.txt'));
+print(random_line('../edited/Law notes.txt'));
