a0cfb72a9d2ad792f46daff33ef177baf8a2beae
flywheel <flywheel@flywheels-MacBook-Pro.local>
2020-02-01 23:23:31 -0800

    Random Collection saved to text file with command grouping

---
+++ scripts/randomcollection.sh
@@ -0,0 +1,1 @@
+ { python getrandomline.py  ; ruby getrandomline.rb ; go run getrandomline.go; ./getrandomline; awk -f getrandomline.awk ../edited/lawnotes.txt; cargo run ../edited/lawnotes.txt ; sh getrandomline.sh ; sh getrandomline2.sh ../edited/lawnotes.txt; java GetRandomLine;  } > compendium.txt;
